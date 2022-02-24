77d80d4d7367c7301ffc8295c878eae5abb4853a
pedrovic7997 <pedrovictor6974@gmail.com>
2022-02-24 20:55:53 -0300

    Exportando imagens das ASTs

--- runall.sh
+++ runall.sh
@@ -16,10 +16,10 @@ for infile in `ls $IN/*.go`; do
     base=$(basename $infile)
     outfile=$OUT/${base/.go/.out}
     dotfile=$OUT/${base/.go/.dot}
-    pdffile=$OUT/${base/.go/.pdf}
+    pngfile=$OUT/${base/.go/.png}
     echo Running $base
     mvn exec:java -Dexec.args="$infile"  1> $outfile 2> $dotfile
-    dot -Tpdf $dotfile -o $pdffile
+    dot -Tpng $dotfile -o $pngfile
 done
 
 echo; echo ---Running with errors---; echo
@@ -29,8 +29,8 @@ for infile in `ls $ERRORIN/*.go`; do
     base=$(basename $infile)
     outfile=$ERROROUT/${base/.go/.out}
     dotfile=$ERROROUT/${base/.go/.dot}
-    pdffile=$ERROROUT/${base/.go/.pdf}
+    pngfile=$OUT/${base/.go/.png}
     echo Running $base
     mvn exec:java -Dexec.args="$infile"  1> $outfile 2> $dotfile
-    dot -Tpdf $dotfile -o $pdffile
+    dot -Tpng $dotfile -o $pngfile
 done
