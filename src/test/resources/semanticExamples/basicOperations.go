33e349666b7427b7e78731c72bbe7cef449c581c
Matheus Lenke <matheuslenke@gmail.com>
2022-02-24 21:11:15 -0300

    Atualizando caso de teste BasicOperations

--- src/test/resources/semanticExamples/basicOperations.go
+++ src/test/resources/semanticExamples/basicOperations.go
@@ -12,5 +12,20 @@ func testeOperacao() {
 	var eq bool = int1 == int2
 	var lt bool = int1 < int2
 	var gt bool = int1 > int2
+	var let bool = int1 <= int2
+	var get bool = int1 >= int2
+
+	var float1 float32 = 15.0
+	var float2 float32 = 20.0
+
+	var add2 float32 = float1 + float2
+	var sub2 float32 = float1 - float2
+	var mul2 float32 = float1 * float2
+	var div2 float32 = float1 / float2
+
+	var string1 string = "Hello"
+	var string2 string = "World"
+
+	var add3 string = string1 + string2
 
 }
